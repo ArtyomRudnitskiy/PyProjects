420a21f2661986d2dc49809919289b77bd0aa25a
artem_0026 <galaktuss@yandex.ru>
2022-05-11 16:56:10 +0300

    pet and dog classes added

--- oop/inheritance/one.py
+++ oop/inheritance/one.py
@@ -1,1 +1,22 @@
-print("one")
+class Pet:
+    def __init__(self, name=None):
+        self._name = name
+
+
+class Dog(Pet):
+    def __init__(self, name, breed=None):
+        super().__init__(name)  # вызываем инициализатор у родителя, по-другому нельзя
+        self._breed = breed
+
+    def say(self):
+        return "{}: waw".format(self._name)
+
+    def get_breed(self):
+        return "{}'s порода - {}".format(self._name, self._breed)
+
+
+if __name__ == '__main__':
+    dog = Dog("Шарик", "Доберман")
+    print(dog.say())
+    print(dog.get_breed())
+
