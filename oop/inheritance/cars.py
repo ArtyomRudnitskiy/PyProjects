290679c04fb151fde225121beebb4aa9f8d94ddd
artem_0026 <galaktuss@yandex.ru>
2022-05-13 11:03:26 +0300

    Everything seems to be working

--- oop/inheritance/cars.py
+++ oop/inheritance/cars.py
@@ -1,4 +1,5 @@
 import os
+import csv
 
 
 class CarBase:
@@ -6,7 +7,7 @@ class CarBase:
         extensions = (".jpg", ".jpeg", ".png", ".gif")
         spl_file_name = os.path.splitext(photo_file_name)
         if spl_file_name[1] not in extensions:
-            raise NameError("Wrong extension")
+            raise AttributeError("Wrong extension")
 
         self._car_type = car_type
         self._brand = brand
@@ -32,6 +33,35 @@ class Truck(CarBase):
         return self._body_length * self._body_width * self._body_height
 
 
+class Car(CarBase):
+    def __init__(self, brand, photo_file_name, carrying, passenger_seats_count):
+        super().__init__("Car", brand, photo_file_name, carrying)
+        self._passenger_seats_count = passenger_seats_count
+
+
+class SpecMachine(CarBase):
+    def __init__(self, brand, photo_file_name, carrying, extra):
+        super().__init__("SpecMachine", brand, photo_file_name, carrying)
+        self._extra = extra
+
+
+def get_car_list(csv_filename):
+    car_list = []
+
+    with open(csv_filename) as csv_fd:
+        reader = csv.reader(csv_fd, delimiter=';')
+        next(reader)  # пропускаем заголовок
+        for row in reader:
+            if row and row[1] and row[3] and row[5]:
+                if row[0] == "truck":
+                    car_list.append(Truck(row[1], row[3], row[5], row[4]))
+                if row[0] == "car" and row[2]:
+                    car_list.append(Car(row[1], row[3], row[5], row[2]))
+                if row[0] == "spec_machine" and row[6]:
+                    car_list.append(SpecMachine(row[1], row[3], row[5], row[6]))
+
+    return car_list
+
+
 if __name__ == '__main__':
-    car = CarBase(1, 2, "dfsdfs.jpeg", 4)
-    print(car.get_photo_file_ext())
+    print(get_car_list("D:\Download Yandex\cars.csv"))
