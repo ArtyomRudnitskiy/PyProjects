4c8ff150dca307fc7d28eb744aa7929d0754cf44
artem_0026 <galaktuss@yandex.ru>
2022-05-19 01:55:48 +0300

    tkinter buttons lesson is finished

--- tkinter_lib/button(3).py
+++ tkinter_lib/button(3).py
@@ -1,9 +1,76 @@
 import tkinter as tk
 
+
+def say_hello():
+    print("Hello!")
+
+
+def add_label():
+    lbl = tk.Label(root, text="New label")
+    lbl.pack()
+
+
+def counter_increment():
+    global counter
+    counter += 1
+    btn4["text"] = f"Counter: {counter}"
+
+
+def disable_buttons():
+    if btn1["state"] == tk.DISABLED:
+        for btn in buttons_list:
+            btn["state"] = tk.NORMAL
+    else:
+        for btn in buttons_list:
+            btn["state"] = tk.DISABLED
+
+
 root = tk.Tk()
 root.geometry("400x400")
 root.title("My first GUI app")
+buttons_list = []
 
+# first button "greater"
+btn1 = tk.Button(root, text="PUSH ME",
+                 command=say_hello
+                 )
+btn1.pack()
+buttons_list.append(btn1)
+
+# second button "label adder"
+btn2 = tk.Button(root, text="DON'T PUSH ME",
+                 command=add_label
+                 )
+btn2.pack()
+buttons_list.append(btn2)
+
+# third button "lambder"
+btn3 = tk.Button(root, text="LAMBDA",
+                 command=lambda: tk.Label(root, text="New lambda").pack()
+                 )
+btn3.pack()
+buttons_list.append(btn3)
+
+# forth button "incrementer"
+counter = 0
+btn4 = tk.Button(root, text=f"Counter: {counter}",
+                 command=counter_increment,
+                 bg="green",
+                 activebackground="blue",
+                 fg="white",
+                 activeforeground="white",
+                 state=tk.NORMAL,
+                 )
+btn4.pack()
+buttons_list.append(btn4)
+
+# fifth button "disabler"
+btn5 = tk.Button(root, text="Disable all the buttons",
+                 command=disable_buttons,
+                 bg="red",
+                 fg="white"
+                 )
+btn5.pack()
 
 
 root.mainloop()
