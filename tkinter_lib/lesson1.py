61611c31cd9f895d3b45944a400b9ef1b96e1d7f
artem_0026 <galaktuss@yandex.ru>
2022-05-19 00:12:55 +0300

    first ktinker lesson finished

--- tkinter_lib/lesson1.py
+++ tkinter_lib/lesson1.py
@@ -1,6 +1,26 @@
 import tkinter as tk
 
-root = tk.Tk()  # create the main window
+# create the main window and set title
+root = tk.Tk()
+root.title("My first GUI app")
 
-root.mainloop()
+# 2 versions to set geometry:
+# root.geometry("400x400+100+200")
+# or
+h = w = 400
+root.geometry(f"{h}x{w}+100+200")
+
+# resizing settings
+root.minsize(200, 200)
+root.maxsize(600, 600)
+root.resizable(True, True)
+
+# change icon
+icon = tk.PhotoImage(file="code.png")
+root.iconphoto(False, icon)
+
+# change background
+root.config(bg="#f0933c")
+
+root.mainloop()  # open mw
 
